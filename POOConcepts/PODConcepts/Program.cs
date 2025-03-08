0675f0c3df4126f6eb61946250c6b985d7599119
Luis Miguel Lopez Moreno <luiso.lo2704@gmail.com>
2025-03-08 11:09:33 -0500

    Date Class

---
+++ POOConcepts/PODConcepts/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PODConcepts;
+
+try
+{
+
+    var date1 = new Date();
+    var date2 = new Date(1974, 12, 5);
+    var date3 = new Date(2019, 2, 29);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+   
